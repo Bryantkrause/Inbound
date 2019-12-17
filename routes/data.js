47f7571033801215c4a462487aab4f50d193ce80
Bryant Krause <bkrause@ttdwest.local>
2019-12-17 12:20:28 -0800

    initial routes for order

--- routes/data.js
+++ routes/data.js
@@ -1,4 +1,9 @@
+const {Order} = require('../models')
 
 module.exports = app =>{
-    
+    app.get('/order', (req, res) =>{
+        Order.find()
+        .then(order => res.json(order))
+        .catch(e => console.log(e))
+    })
 }
