4fd6b34e29ffd94bd6b8a728e7d8100432875441
Bryant Krause <bkrause@ttdwest.local>
2019-12-17 10:15:31 -0800

    adding mcv

--- server.js
+++ server.js
@@ -4,3 +4,17 @@
 
 // web reqs
 // https://docs.microsoft.com/en-us/office/dev/add-ins/excel/custom-functions-web-reqs
+
+const express = require('express')
+const app = express()
+const {join} =require('path')
+
+app.use(express.static(join(_dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+require('./routes')(app)
+
+
+
+app.listen(3000)
